921919efd1aac29d31b431abf31ea40f7f9c3567
AurelienAllenic <aurelien.allenic@gmail.com>
2024-04-09 19:18:47 +0200

    (fix) changed style of main title projects to not be under the other section programmation

--- src/components/Portfolio/PythonFormation.jsx
+++ src/components/Portfolio/PythonFormation.jsx
@@ -7,10 +7,10 @@ const PythonFormation = () => {
   const { language } = useLanguage();
   return (
     <>
-    <p className='main-title-portfolio_projects margin'>{language === 'FR' ? 'Projets' : 'Projects'}</p>
       <a className='anchor' id='formation-python'></a>
-      <section className="container_portfolio" id="formation-react">
-        <h1 className="react main-title-portfolio">{language === 'FR' ? 'Formation Python' : 'Python formation'}</h1>
+      <section className="container_portfolio margin" id="formation-react">
+        <h1 className='react first-title'>{language === 'FR' ? 'Projets' : 'Projects'}</h1>
+        <h1 className="main-title-portfolio">{language === 'FR' ? 'Formation Python' : 'Python formation'}</h1>
           <article className="container-projects" >
             <CardSection datas={openclassrooms3} />
           </article>
